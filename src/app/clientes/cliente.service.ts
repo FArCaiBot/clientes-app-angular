cbe887bb2939005a798375f3c49c61a528f59790
FArCaiBot <flavio397david@gmail.com>
2023-04-20 17:26:25 -0500

    conect backend

--- src/app/clientes/cliente.service.ts
+++ src/app/clientes/cliente.service.ts
@@ -1,13 +1,18 @@
 import { Injectable } from '@angular/core';
 import { CLIENTES } from './clientes.json';
 import { Cliente } from './cliente';
-import { Observable, of } from 'rxjs';
+import { Observable, map, of } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable()
 export class ClienteService {
-  constructor() {}
+  private urlEndPoint: string = 'http://localhost:8080/cliente';
+  constructor(private http: HttpClient) {}
 
   getClientes(): Observable<Cliente[]> {
-    return of(CLIENTES);
+    return this.http
+      .get(this.urlEndPoint)
+      .pipe(map((response) => response as Cliente[]));
+    //return of(CLIENTES);
   }
 }
